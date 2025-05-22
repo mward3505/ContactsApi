6a94e681fca5d3c4834ce5e59b8d32cfcef7b7a9
Matt Ward <matt.ward3505@gmail.com>
2025-05-22 15:07:43 -0600

    Added Validation and error messages for contact fields

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,10 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContactsApi.Models;
 
 public class Contact
 {
     public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required(ErrorMessage = "First name is required")]
     public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Last name is required")]
     public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
     public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Phone number is required")]
+    [Phone(ErrorMessage = "Invalid phone number")]
     public string Phone { get; set; } = string.Empty;
 }
